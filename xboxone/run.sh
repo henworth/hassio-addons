6426ef43bff3b005ec8135a4f10f72365a2597b3
Mike Hennessy <henworth@henabytes.com>
2019-06-12 13:29:08 -0400

    Add init file to component

--- xboxone/run.sh
+++ xboxone/run.sh
@@ -3,12 +3,12 @@
 # Copy over the xbox component
 mkdir -p /config/custom_components/xboxone
 cp -f xboxone.py /config/custom_components/xboxone/media_player.py
+echo '"""The Xbox One component."""' > /config/custom_components/xboxone/__init__.py
 
 # Persistent tokens on reboot
 touch /config/.xbox-token.json
 mkdir -p /root/.local/share/xbox/
 ln -s /config/.xbox-token.json /root/.local/share/xbox/tokens.json
-
 
 # Run the server
 xbox-rest-server
